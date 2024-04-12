62581c76c19b2f02e9956c68b60c98e53088a5fc
Satyam singh <satyamsingh2003a@gmail.com>
2024-04-12 21:32:46 +0530

    done

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import Codemirror from 'codemirror';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/dracula.css';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/addon/edit/closetag';
-import 'codemirror/addon/edit/closebrackets';
-import ACTIONS from '../Actions';
+
 
 const Editor = ({ socketRef, roomId, onCodeChange }) => {
     const editorRef = useRef(null);
